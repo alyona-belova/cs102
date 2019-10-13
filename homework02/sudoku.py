5685180cc702cf3e84a96688082655531048344d
Alyona Belova <magic.near@yandex.ru>
2019-10-13 14:22:04 +0300

    Реализована функция solve()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -182,7 +182,16 @@ def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    pos = find_empty_positions(grid)
+    if pos == None:
+        return grid
+    row, col = pos
+    for value in find_possible_values(grid, pos):
+        grid[row][col] = value
+        solution = solve(grid)
+        if solution != None:
+            return solution
+    grid[row][col] = '.'
 
 
 def check_solution(solution: List[List[str]]) -> bool:
