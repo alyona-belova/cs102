2e65d03017870bfbb19aa99ad7db1f2b3349db7b
Alyona Belova <magic.near@yandex.ru>
2019-10-27 16:43:09 +0300

    Реализованы функции create_grid(), get_neighbours(), get_next_generation(), step()

--- homework03/life.py
+++ homework03/life.py
@@ -30,21 +30,61 @@ class GameOfLife:
 
     def create_grid(self, randomize: bool=False) -> Grid:
         # Copy from previous assignment
-        pass
+        grid = [[None] * self.cell_width for _ in range(self.cell_height)]
+        if randomize != 0:
+            for i in range(self.cell_height):
+                for j in range(self.cell_width):
+                    grid[i][j] = random.randint(0, 1)
+        else:
+            for i in range(self.cell_height):
+                for j in range(self.cell_width):
+                    grid[i][j] = 0
+        return grid
 
     def get_neighbours(self, cell: Cell) -> Cells:
         # Copy from previous assignment
-        pass
+        row, col = cell
+        cells = []
+        if col > 0:
+            cells.append(self.grid[row][col - 1])
+            if row > 0:
+                cells.append(self.grid[row - 1][col - 1])
+            if row < (self.cell_height - 1):
+                cells.append(self.grid[row + 1][col - 1])
+        if col < (self.cell_width - 1):
+            cells.append(self.grid[row][col + 1])
+            if row > 0:
+                cells.append(self.grid[row - 1][col + 1])
+            if row < (self.cell_height - 1):
+                cells.append(self.grid[row + 1][col + 1])
+        if row > 0:
+            cells.append(self.grid[row - 1][col])
+        if row < (self.cell_height - 1):
+            cells.append(self.grid[row + 1][col])
+        return cells
 
     def get_next_generation(self) -> Grid:
         # Copy from previous assignment
-        pass
+        dead = []
+        alive = []
+        for i in range(self.cell_height):
+            for j in range(self.cell_width):
+                if (sum(self.get_neighbours((i, j))) == 3) and (self.grid[i][j] == 0):
+                    alive.append((i, j))
+                elif (sum(self.get_neighbours((i, j))) < 2) or (sum(self.get_neighbours((i, j))) > 3):
+                    dead.append((i, j))
+        for i in alive:
+            self.grid[i[0]][i[1]] = 1
+        for i in dead:
+            self.grid[i[0]][i[1]] = 0
+        return self.grid
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        pass
+        self.get_next_generation()
+        self.generations += 1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
