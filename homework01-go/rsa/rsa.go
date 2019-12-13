1251fe86ef5ca740b845b0013a1df5c7994152fb
Alyona Belova <55944529+Magic-near@users.noreply.github.com>
2019-12-14 00:38:21 +0300

    Добавлен rsa.go

---
+++ homework01-go/rsa/rsa.go
@@ -0,0 +1,110 @@
+package rsa
+
+import (
+	"errors"
+	"math"
+	"math/big"
+	"math/rand"
+)
+
+//Key key
+type Key struct {
+	key, n int
+}
+
+//KeyPair keypair
+type KeyPair struct {
+	Private, Public Key
+}
+
+func isPrime(n int) bool {
+	for factor := 2; factor < int(math.Sqrt(float64(n)))+1; factor++ {
+		if n%factor == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func gcd(a int, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+func multiplicativeInverse(e int, phi int) int {
+	var x int
+
+	var history []int
+
+	for e%phi > 0 {
+		history = append(history, int(e/phi))
+		e, phi = phi, e%phi
+	}
+
+	x = 0
+	y := 1
+
+	for i := len(history) - 1; i >= 0; i-- {
+		prevX := x
+		x = y
+		y = prevX - y*(history[i])
+	}
+
+	return x
+}
+
+//GenerateKeypair generates keypair
+func GenerateKeypair(p int, q int) (KeyPair, error) {
+	var n int
+	var phi int
+	if !isPrime(p) || !isPrime(q) {
+		return KeyPair{}, errors.New("both numbers must be prime")
+	} else if p == q {
+		return KeyPair{}, errors.New("p and q can't be equal")
+	}
+
+	n = p * q
+
+	phi = (p - 1) * (q - 1)
+
+	e := rand.Intn(phi-1) + 1
+	g := gcd(e, phi)
+
+	for g != 1 {
+		e = rand.Intn(phi-1) + 1
+		g = gcd(e, phi)
+	}
+
+	d := multiplicativeInverse(e, phi)
+
+	return KeyPair{Key{e, n}, Key{d, n}}, nil
+}
+
+//Encrypt encrypts
+func Encrypt(pk Key, plaintext string) []int {
+	cipher := []int{}
+	n := new(big.Int)
+	for _, ch := range plaintext {
+		n = new(big.Int).Exp(
+			big.NewInt(int64(ch)), big.NewInt(int64(pk.key)), nil)
+		n = new(big.Int).Mod(n, big.NewInt(int64(pk.n)))
+		cipher = append(cipher, int(n.Int64()))
+	}
+	return cipher
+}
+
+//Decrypt decrypts
+func Decrypt(pk Key, cipher []int) string {
+	plaintext := ""
+	n := new(big.Int)
+	for _, ch := range cipher {
+		n = new(big.Int).Exp(
+			big.NewInt(int64(ch)), big.NewInt(int64(pk.key)), nil)
+		n = new(big.Int).Mod(n, big.NewInt(int64(pk.n)))
+		plaintext += string(rune(int(n.Int64())))
+	}
+	return plaintext
+}
