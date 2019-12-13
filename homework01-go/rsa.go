5a950c53a1d3b800a9dadd984fd7f12b562ee232
Alyona Belova <55944529+Magic-near@users.noreply.github.com>
2019-12-14 00:21:46 +0300

    Исправлен rsa.go

--- homework01-go/rsa.go
+++ homework01-go/rsa.go
@@ -20,75 +20,69 @@ type KeyPair struct {
 }
 
 func isPrime(n int) bool {
-	if n == 2 || n == 3 {
-		return true
-	}
 
-	if n < 2 || n%2 == 0 {
-		return false
-	}
-
-	for i := 2; i <= int(math.Floor(math.Sqrt(float64(n)))); i++ {
-		if n%i == 0 {
+	for factor := 2; factor < int(math.Sqrt(float64(n)))+1; factor++ {
+		if n%factor == 0 {
 			return false
 		}
 	}
+
 	return true
 }
 
 func gcd(a int, b int) int {
-	for b != 0 {
-		a, b = b, a%b
+	if b == 0 {
+		return a
 	}
-	return a
+	return gcd(b, a%b)
 }
 
 func multiplicativeInverse(e int, phi int) int {
-	var tbl [][]int
+	var x int
 
-	A := phi
-	B := e
-	row := []int{A, B, A % B, A / B, -1, -1}
-	tbl = append(tbl, row)
-	for i := 0; tbl[i][2] != 0; i++ {
-		A = tbl[i][1]
-		B = tbl[i][2]
-		row := []int{A, B, A % B, A / B, -1, -1}
-		tbl = append(tbl, row)
+	var history []int
+
+	for e%phi > 0 {
+		history = append(history, int(e/phi))
+		e, phi = phi, e%phi
 	}
 
-	tbl[len(tbl)-1][4] = 0
-	tbl[len(tbl)-1][5] = 1
-	for i := len(tbl) - 2; i >= 0; i-- {
-		tbl[i][4] = tbl[i+1][5]
-		tbl[i][5] = tbl[i+1][4] - tbl[i+1][5]*tbl[i][3]
+	x = 0
+	y := 1
+
+	for i := len(history) - 1; i >= 0; i-- {
+		prevX := x
+		x = y
+		y = prevX - y*(history[i])
 	}
 
-	r := tbl[0][5] % phi
-	if r < 0 {
-		r = r + phi
-	}
-	return r
+	return x
 }
 
 //GenerateKeypair generates keypair
 func GenerateKeypair(p int, q int) (KeyPair, error) {
+	var n int
+	var phi int
 	if !isPrime(p) || !isPrime(q) {
 		return KeyPair{}, errors.New("both numbers must be prime")
 	} else if p == q {
 		return KeyPair{}, errors.New("p and q can't be equal")
 	}
 
-	n := p * q
-	phi := (p - 1) * (q - 1)
+	n = p * q
+
+	phi = (p - 1) * (q - 1)
+
 	e := rand.Intn(phi-1) + 1
 	g := gcd(e, phi)
+
 	for g != 1 {
 		e = rand.Intn(phi-1) + 1
 		g = gcd(e, phi)
 	}
 
 	d := multiplicativeInverse(e, phi)
+
 	return KeyPair{Key{e, n}, Key{d, n}}, nil
 }
 
